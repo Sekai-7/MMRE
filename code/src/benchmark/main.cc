5bede83798d09ac684ce8d9d01e3ff582b376c3a
Sekai <1252649225@qq.com>
2025-08-21 22:18:04 +0800

    main

--- code/src/benchmark/main.cc
+++ code/src/benchmark/main.cc
@@ -1,16 +1,43 @@
 #include <benchmark/benchmark.h>
 #include <vector>
 
+#include "TimelineCacheBenchmark.h"
+
 using std::vector;
 
-static void benchmark_test(benchmark::State& state) {
+// sample code
+static void sample_test(benchmark::State& state) {
+    vector<int> vec;
     for (auto _ : state) {
-        vector<int> vec;
-        for (int i = 0; i < 100; ++i)
+        vec.clear();
+        for (int i = 0; i < 10000; ++i)
             vec.push_back(i);
     }
 }
 
-BENCHMARK(benchmark_test);
+static void benchmark_insert_test(benchmark::State& state) {
+    for (auto _ : state) {
+        TimelineCacheBaseLine tcb{};
+        char data[]{"data"};
+        for (int i = 0; i < 10000; ++i)
+            tcb.insert(i, DataType::CAMERA, data, sizeof(data));
+    }
+}
+
+static void benchmark_find_test(benchmark::State& state) {
+    for (auto _ : state) {
+        state.PauseTiming();
+        TimelineCacheBaseLine tcb{};
+        char data[]{"data"};
+        for (int i = 0; i < 10000; ++i)
+            tcb.insert(i, DataType::CAMERA, data, sizeof(data));
+        state.ResumeTiming();
+        for (int i = 0; i < 10000; ++i)
+            tcb.find(i);
+    }
+}
+
+BENCHMARK(benchmark_insert_test)->Unit(benchmark::kMillisecond);
+BENCHMARK(benchmark_find_test)->Unit(benchmark::kMillisecond);
 
 BENCHMARK_MAIN();
