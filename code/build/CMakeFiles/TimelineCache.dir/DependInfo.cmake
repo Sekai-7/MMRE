c665ed535743c3d47e38de266b6399d87061d5d3
Sekai <1252649225@qq.com>
2025-08-21 22:28:54 +0800

    baseline without mutex

---
+++ code/build/CMakeFiles/TimelineCache.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/Use/TimeLineCache/code/src/benchmark/TimelineCacheBenchmark.cc" "/root/Use/TimeLineCache/code/build/CMakeFiles/TimelineCache.dir/src/benchmark/TimelineCacheBenchmark.cc.o"
+  "/root/Use/TimeLineCache/code/src/benchmark/main.cc" "/root/Use/TimeLineCache/code/build/CMakeFiles/TimelineCache.dir/src/benchmark/main.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BENCHMARK_STATIC_DEFINE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../inc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ code/build/CMakeFiles/TimelineCache.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TimelineCache.dir/src/benchmark/TimelineCacheBenchmark.cc.o"
+  "CMakeFiles/TimelineCache.dir/src/benchmark/main.cc.o"
+  "TimelineCache"
+  "TimelineCache.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TimelineCache.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
